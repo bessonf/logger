c2d7d275d61f31a648ee6048022d529fd43b8d79
Frank Besson <32044707+frank-besson@users.noreply.github.com>
2022-05-12 14:40:36 -0400

    Update logger.py

--- logger.py
+++ logger.py
@@ -4,40 +4,40 @@ from datetime import datetime
 cwd = os.path.dirname(os.path.realpath(__file__))
 now_fname = f'{datetime.now().strftime("%m-%d-%Y_%H:%M:%S")}.txt'
 
-# START: HANDLE PATHS FOR ERROR FILES
+
+# HANDLE PATHS FOR ERROR FILES
 err_path = os.path.join(cwd, '_err')
 
 if not os.path.exists(err_path):
     os.makedirs(err_path)
 
 err_fname = os.path.join(err_path, now_fname)
-# END: HANDLE PATHS FOR ERROR FILES
 
 
-# START: HANDLE PATHS FOR LOG FILES
+# HANDLE PATHS FOR LOG FILES
 log_path = os.path.join(cwd, '_log')
 
 if not os.path.exists(log_path):
     os.makedirs(log_path)
 
 log_fname = os.path.join(log_path, now_fname)
-# END: HANDLE PATHS FOR LOG FILES
 
 
-# START: DEFINE FORMATTER AND FILTER
+# DEFINE FORMATTER
 formatter = logging.Formatter(
     '%(asctime)s | %(name)s |  %(levelname)s: %(message)s')
 
+
+# DEFIN FILTER
 class MyFilter(object):
     def __init__(self, level):
         self.__level = level
 
     def filter(self, logRecord):
         return logRecord.levelno <= self.__level
-# END: DEFINE FORMATTER AND FILTER
 
 
-# START: DFFINE HANDLER FOR INFO LEVEL
+# DFFINE HANDLER FOR INFO LEVEL
 handler_log = logging.FileHandler(
     filename=log_fname,
     mode='w',
@@ -46,10 +46,9 @@ handler_log = logging.FileHandler(
 handler_log.setFormatter(formatter)
 handler_log.setLevel(logging.INFO)
 handler_log.addFilter(MyFilter(logging.INFO))
-# END: DFFINE HANDLER FOR INFO LEVEL
 
 
-# START: DFFINE HANDLER FOR ERROR LEVEL
+# DFFINE HANDLER FOR ERROR LEVEL
 handler_err = logging.FileHandler(
     filename=err_fname,
     mode='w',
@@ -58,10 +57,9 @@ handler_err = logging.FileHandler(
 handler_err.setFormatter(formatter)
 handler_err.setLevel(logging.ERROR)
 handler_err.addFilter(MyFilter(logging.ERROR))
-# END: DFFINE HANDLER FOR ERROR LEVEL
 
 
-# START: DFFINE LOGGER
+# DFFINE LOGGER
 logging.basicConfig(
 	handlers=[
 		# logging.StreamHandler(),
@@ -74,7 +72,6 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger('my_logger')
-# END: DFFINE LOGGER
 
 
 if __name__ == "__main__":
